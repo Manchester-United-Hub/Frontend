41f83c4e919961c60e39772e22106c5edbe97e0f
L1m3Kun <sungku2757@gmail.com>
2026-07-27 16:32:12 +0900

    fix: align match data with api-sports response #43
    ## 설명
    경기 일정 데이터 소스가 API-Sports로 바뀌면서 응답 형식과 어긋난 부분을 맞췄습니다. API-Sports는 팀명에 "FC" 접미를 붙이지 않아 팀 코드 매핑이 전부 실패했고, 팀 로고가 api-sports.io 도메인에서 내려와 next/image가 차단하던 문제를 함께 해결했습니다.
    
    ## 변경 내용
    - PL_TEAM_KOREAN_NAME 매핑 키를 API-Sports 팀명("FC" 접미 없음)으로 교체
    - next.config.ts images.remotePatterns에 **.api-sports.io 허용 추가
    
    ## 관련 이슈
    - #43

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: process.env.NETLIFY ? undefined : 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.api-sports.io',
+        pathname: '**/*',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
